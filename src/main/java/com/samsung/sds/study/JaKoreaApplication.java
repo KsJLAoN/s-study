c15e806eae6db7374f207a44d37b112e34a404b6
unknown <kangsojung0728@gmail.com>
2023-06-08 21:45:42 +0900

    first commit

---
+++ src/main/java/com/samsung/sds/study/JaKoreaApplication.java
@@ -0,0 +1,13 @@
+package com.samsung.sds.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaKoreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaKoreaApplication.class, args);
+	}
+
+}
